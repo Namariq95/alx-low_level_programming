5c62c1b683b155b168840c0d27665e3edfad8990
Namariq <Namariq95@gmail.com>
2023-08-10 00:32:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<srdio.h>
+
+
+/*
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Alwways 0.
+ *
+ */
+int main(void)
+
+{
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
