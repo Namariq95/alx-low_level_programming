352c0a70214d81c1691e97dcce3fa9e3b7810fff
Namariq <Namariq95@gmail.com>
2023-08-17 18:59:32 -0700

    print 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ * Bwave ICT file
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+			_putchar(1);
+			}
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+	}
+}
