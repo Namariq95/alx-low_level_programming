e998d7fe07f153688aa7c37ce41a59c3e60bcd1a
Namariq <nemonmarig95@gmail.com>
2023-08-28 18:05:26 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,27 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *  * leet - Write a function that encodes a string into 1337
- *   *
- *    * @changed: This is the input string
- *     *
- *      * Return: String converted to 1337
- *       */
-
-char *leet(char *changed)
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	int index, j;
-	char minus[] = {'a', 'e', 'o', 't', 'l', '\0'};
-	char mayus[] = {'A', 'E', 'O', 'T', 'L', '\0'};
-	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-	for (index = 0; changed[index] != '\0'; ++index)
-	{
-	for (j = 0; j < 5; j++)
-	{
-																if (changed[index] == minus[j] || changed[index] == mayus[j])								{
-										 						changed[index] = numbers[j];																											}
-	}
-	}
-	return (changed);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
