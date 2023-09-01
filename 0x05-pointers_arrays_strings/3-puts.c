5ca37487ab4f56ae4403cf840221d661acd3291c
Namariq <nemonmarig95@gmail.com>
2023-09-01 12:39:42 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * _puts - prints a string
+ *_puts - prints a string
+ * @str: string to print
  *
- * @str: The string to print
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
+
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-	_putchar(*str);
-	}
-	_putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
