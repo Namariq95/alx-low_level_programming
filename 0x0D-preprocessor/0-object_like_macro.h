cd578fc264d6f9409ae9f8ad637450945595262f
Namariq <nemonmarig95@gmail.com>
2023-09-11 01:20:51 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+/**
+ * file: 0-object_like_macro
+ *
+ * Deso: header file
+ */
+#define SIZE 1024
+
+#endif /* _OBJECT_LIKE_MACRO_H */
